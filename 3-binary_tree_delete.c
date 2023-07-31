474818f1b9514094f4d9af350b656f1c74766dde
thomaskitaba <thomaskitaba@gmail.com>
2023-07-31 21:27:25 +0300

    update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,38 +1,19 @@
 # include "binary_trees.h"
 # include <stdlib.h>
 /**
-* binary_tree_insert_left - create a binary tree node
-* @parent: parent node
-* @value: value for the new node
-* Return: pointer to the new_left node created
+* binary_tree_delete - create a binary tree node
+* @tree: parent node
+* Return: NULL
 */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+void binary_tree_delete(binary_tree_t *tree)
 {
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
-	binary_tree_t *new_left;
+	if (tree == NULL)
+		return;
+	/* we used postorder traversal to delete our tree */
+	/* it is taken as the best root to delete a binary tree */
+	/* without any additional overhead code */
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 
-	new_left = malloc(sizeof(binary_tree_t));
-	if (new_left == NULL)
-		return (NULL);
-
-	new_left->parent = parent;
-	new_left->left = NULL;
-	new_left->right = NULL;
-	new_left->n = value;
-
-    if (parent->left != NULL)
-    {
-        /* make new_left left node point to old left node*/
-        new_left->left = parent->left;
-        /* make the old left parent point to the inserted left*/
-        parent->left->parent = new_left;
-    }
-	/* make the parent point to the created left node*/
-	parent->left = new_left;
-
-	/* return the created left node*/
-	return (new_left);
 }
