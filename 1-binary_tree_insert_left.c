ab16ab470b48a6c111c1836301d7bd2097326739
thomaskitaba <thomaskitaba@gmail.com>
2023-08-01 22:44:52 +0300

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,23 +9,23 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_left;
 
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
+	new_left = binary_tree_node(parent, value);
+	if (new_left == NULL)
 	{
 		return (NULL);
 	}
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		new_left->left = parent->left;
+		parent->left->parent = new_left;
 	}
-	parent->left = new_node;
-	return (new_node);
+	parent->left = new_left;
+	return (new_left);
 }
