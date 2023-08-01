e7faebb3fed9c1b11261fc217506c9a6f1ea26d0
thomaskitaba <thomaskitaba@gmail.com>
2023-08-01 22:24:55 +0300

    1-binary tree insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,21 +13,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new_left = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (new_left == NULL)
-		return (NULL);
-
-	new_left->n = value;
-	new_left->left = NULL;
-	new_left->right = NULL;
-	new_left->parent = parent;
-
+	new_left = binary_tree_node(parent, value);
+    if (new_left == NULL)
+    {
+        return (NULL);
+    }
 	if (parent->left != NULL)
 	{
-		new_left->right = parent->left;
+		new_left->left = parent->left;
 		parent->left->parent = new_left;
 	}
-
-	parent->right = new_left;
+	parent->left = new_left;
 	return (new_left);
 }
