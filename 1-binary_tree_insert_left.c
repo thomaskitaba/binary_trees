b2b747de168d4a0591d3832a25a2251804fee068
thomaskitaba <thomaskitaba@gmail.com>
2023-08-01 20:59:19 +0300

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	binary_tree_t *new_left;
 
-	new_left = malloc(sizeof(binary_tree_t));
+	new_left = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (new_left == NULL)
 		return (NULL);
 
