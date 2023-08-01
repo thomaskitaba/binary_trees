3766f1ddc1fe5c4559851be14882cf3389fd0cbe
thomaskitaba <thomaskitaba@gmail.com>
2023-08-01 21:37:34 +0300

    1-binary

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,11 +8,13 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+    binary_tree_t *new_left;
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
-	binary_tree_t *new_left;
+
 
 	new_left = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (new_left == NULL)
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,30 @@
 # include "binary_trees.h"
 # include <stdlib.h>
 /**
+* binary_tree_node - create a binary tree node
+* @parent: parent node
+* @value: value of a parent node
+* Return: pointer to the new_node created
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	/* new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t)) */
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	/* *new_node.parent = parent; */
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+
+	return (new_node);
+}
+/**
 * binary_tree_insert_right - create a binary tree node
 * @parent: parent node
 * @value: value for the new node
