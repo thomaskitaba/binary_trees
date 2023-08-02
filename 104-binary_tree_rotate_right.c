4fb35cce0cc40e48dfc17fa83d2fd7a44fb8a199
thomaskitaba <thomaskitaba@gmail.com>
2023-08-02 23:03:30 +0300

    104-rotr

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,25 @@
+# include "binary_trees.h"
+/**
+* binary_tree_rotate_left - invert tree to left
+* @tree: root of the tree
+* Return: pointer to the root of the inverted root
+*/
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *temp;
+
+	if (tree == NULL || tree->left == NULL)
+		return (NULL);
+
+	temp = tree->left;
+	tree->left = temp->right;
+
+	if (temp->right != NULL)
+	{
+		temp->right->parent = tree;
+	}
+	temp->right = tree;
+	temp->parent = tree->parent;
+	tree->parent = temp;
+	return (temp);
+}
