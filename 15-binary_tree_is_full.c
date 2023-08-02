12bc0773c7496a271f5513c4ff0bcd4d88ecd1fe
thomaskitaba <thomaskitaba@gmail.com>
2023-08-02 15:35:57 +0300

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,22 +7,32 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t left_s, right_s, left_h, right_h;
+	size_t left_full, right_full;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-    if (tree->left != NULL && tree->right != NULL)
-    {
-        /* the recursive call will be place here*/
-    }
-    else if (tree->left == NULL && tree->right == NULL)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		/* the recursive call will be place here*/
+		left_full = binary_tree_is_full(tree->left);
+		right_full = binary_tree_is_full(tree->right);
+		if (left_full == 1 && right_full == 1)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
