dba5fd242ca42962d0b06ab14f9c55f8805a3b2a
thomaskitaba <thomaskitaba@gmail.com>
2023-08-02 19:58:03 +0300

    16-perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,48 +1,6 @@
 # include "binary_trees.h"
 # include <stdlib.h>
-/**
-* binary_tree_depth - depth from sepcfic node to root
-* @tree: root of the binary tree
-* Return: height of a tree
-*/
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	size_t depth;
 
-	depth = 0;
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	while (tree->parent != NULL)
-	{
-		depth++;
-		tree = tree->parent;
-	}
-	return (depth);
-}
-/**
-* binary_tree_leaves - conunt number of leaves
-* @tree: root node of the tree
-* Return: number of leaves
-*/
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	size_t left, right, leaf;
-
-	left = right = leaf = 0;
-	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	left = binary_tree_leaves(tree->left);
-	right = binary_tree_leaves(tree->right);
-	leaf = left + right;
-	if (left == 0 && right == 0)
-		return (leaf + 1);
-	return (leaf);
-}
 /**
 * binary_tree_is_perfect - check if Binary tree is perfect or not
 * @tree: root of the binary tree
@@ -50,19 +8,35 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left, right, leaf, depth;
+	size_t left_full, right_full;
 
-	depth = binary_tree_depth(tree);
-	left = right = leaf = 0;
+    left_full = right_full = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	leaf++;
-	left = binary_tree_leaves(tree->left);
-	right = binary_tree_leaves(tree->right);
+	else if (tree->left != NULL && tree->right != NULL)
+	{
+		/* the recursive call will be place here*/
+		left_full = 1 + binary_tree_is_perfect(tree->left);
+		right_full = 1 + binary_tree_is_perfect(tree->right);
+        if (right_full == left_full)
+        {
+            if (right_full != 0 || left_full != 0)
+            {
+                return (right_full);
+            }
+        }
+    }
 
-	if ((left == 0 && right == 0) && (leaf == depth))
+	else if (tree->left == NULL && tree->right == NULL)
+	{
 		return (1);
-	return (0);
+	}
+	else
+	{
+		return (0);
+	}
+    return (0);
 }
