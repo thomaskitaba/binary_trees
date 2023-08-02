d06cdf66c694e99a74b37466b22680966ab8e063
thomaskitaba <thomaskitaba@gmail.com>
2023-08-02 16:55:34 +0300

    13-binary_tree_nodes.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,38 +1,68 @@
 # include "binary_trees.h"
 # include <stdlib.h>
 /**
+* binary_tree_depth - depth from sepcfic node to root
+* @tree: root of the binary tree
+* Return: height of a tree
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	depth = 0;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
+}
+/**
+* binary_tree_leaves - conunt number of leaves
+* @tree: root node of the tree
+* Return: number of leaves
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t left, right, leaf;
+
+	left = right = leaf = 0;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
+	leaf = left + right;
+	if (left == 0 && right == 0)
+		return (leaf + 1);
+	return (leaf);
+}
+/**
 * binary_tree_is_perfect - check if Binary tree is perfect or not
 * @tree: root of the binary tree
 * Return: 1 if perfect, 0 if not perfect
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_full, right_full;
+    size_t left, right, leaf, depth;
 
+    depth = binary_tree_depth(tree);
+	left = right = leaf = 0;
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	if (tree->left != NULL && tree->right != NULL)
-	{
-		/* the recursive call will be place here*/
-		left_full = binary_tree_is_perfect(tree->left);
-		right_full = binary_tree_is_perfect(tree->right);
-		if (left_full == 1 && right_full == 1)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
-	else if (tree->left == NULL && tree->right == NULL)
-	{
+    leaf++;
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
+
+	if ((left == 0 && right == 0) && (leaf == depth))
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
