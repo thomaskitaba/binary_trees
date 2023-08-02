d7ff302baa6ef675a893f0fc4973022a9f563c23
thomaskitaba <thomaskitaba@gmail.com>
2023-08-02 15:46:09 +0300

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,19 +1,25 @@
 # include "binary_trees.h"
 # include <stdlib.h>
 /**
-* binary_tree_size - number of nodes
-* @tree: root of the binary tree
-* Return: height of a tree
+* binary_tree_sibling - number of nodes
+* @node: node of a binary tree
+* Return: sibling node
 */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	size_t size;
-
-	if (tree == NULL)
+	if (node == NULL || node->parent == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
-
-    /* depth  must be equal,   parent must be the same */
-	return (size);
+	if (node->parent->left == node)
+	{
+		if (node->parent->right)
+			return (node->parent->right);
+		else
+			return (NULL);
+	}
+	if (node->parent->left)
+		return (node->parent->left);
+	else
+		return (NULL);
 }
