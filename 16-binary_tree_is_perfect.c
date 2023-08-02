e815d9d0fde37d17bacad734e30a64a332e3ae04
thomaskitaba <thomaskitaba@gmail.com>
2023-08-02 16:01:38 +0300

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,21 +1,38 @@
 # include "binary_trees.h"
 # include <stdlib.h>
 /**
-* binary_tree_size - number of nodes
+* binary_tree_is_full - number of nodes
 * @tree: root of the binary tree
-* Return: height of a tree
+* Return: 0 on failure , if full BT
 */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t size;
+	size_t left_full, right_full;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size = 1 + binary_tree_size(tree->left);
-	size = size + binary_tree_size(tree->right);
-
-	return (size);
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		/* the recursive call will be place here*/
+		left_full = binary_tree_is_perfect(tree->left);
+		right_full = binary_tree_is_perfect(tree->right);
+		if (left_full == 1 && right_full == 1)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
