728213159606c0d903d117313c25efe4e8dcb572
thomaskitaba <thomaskitaba@gmail.com>
2023-08-02 20:23:33 +0300

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,25 +2,21 @@
 # include <stdlib.h>
 
 /**
-* binary_tree_is_perfect - check if Binary tree is perfect or not
+* tree_is_perfect - check if Binary tree is perfect or not
 * @tree: root of the binary tree
 * Return: 1 if perfect, 0 if not perfect
 */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t left_full, right_full;
 
 	left_full = right_full = 0;
 
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else if (tree->left != NULL && tree->right != NULL)
+	if (tree->left != NULL && tree->right != NULL)
 	{
 		/* the recursive call will be place here*/
-		left_full = 1 + binary_tree_is_perfect(tree->left);
-		right_full = 1 + binary_tree_is_perfect(tree->right);
+		left_full = 1 + tree_is_perfect(tree->left);
+		right_full = 1 + tree_is_perfect(tree->right);
 		if (right_full == left_full)
 		{
 			if (right_full != 0 || left_full != 0)
@@ -33,7 +29,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 			}
 		}
 	}
-
 	else if (tree->left == NULL && tree->right == NULL)
 	{
 		return (1);
@@ -43,4 +38,19 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 	return (0);
+}
+/**
+* binary_tree_is_perfect - check if Binary tree is perfect or not
+* @tree: root of the binary tree
+* Return: 1 if perfect, 0 if not perfect
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree_is_perfect(tree) != 0)
+		return (1);
+	else
+		return (0);
 }
