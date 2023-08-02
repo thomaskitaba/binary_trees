6bab6bf0393e60534aa330bd2f2093275f62cf0a
thomaskitaba <thomaskitaba@gmail.com>
2023-08-02 14:29:44 +0300

    correction to number of leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,10 +1,14 @@
 #include"binary_trees.h"
-
+/**
+* binary_tree_leaves - conunt number of leaves
+* @tree: root node of the tree
+* Return: number of leaves
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t left, right, leaf;
 
-    left = right = leaf = 0;
+	left = right = leaf = 0;
 	if (tree == NULL)
 	{
 		return (0);
@@ -12,8 +16,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	left = binary_tree_leaves(tree->left);
 	right = binary_tree_leaves(tree->right);
-    leaf = left + right;
-    if (left == 0 && right == 0)
-        return (leaf + 1);
-    return (leaf);
+	leaf = left + right;
+	if (left == 0 && right == 0)
+		return (leaf + 1);
+	return (leaf);
 }
