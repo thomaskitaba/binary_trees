077db957a9e650c75241d8770bab8788523060a3
thomaskitaba <thomaskitaba@gmail.com>
2023-08-03 20:54:16 +0300

    update

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,7 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 typedef binary_tree_t bst_t;
+typedef binary_tree_t avl_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
