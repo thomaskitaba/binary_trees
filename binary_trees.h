adea7bdb7c07c600a8acbb80b8ce225cbe8e5225
thomaskitaba <thomaskitaba@gmail.com>
2023-08-03 17:15:35 +0300

    update

--- binary_trees.h
+++ binary_trees.h
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -14,13 +13,16 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -53,4 +55,5 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_complete(const binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *array_to_bst(int *array, size_t size);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 #endif
