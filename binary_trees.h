a7090cfae78eba35c5c6216928df06b73c32aa3a
thomaskitaba <thomaskitaba@gmail.com>
2023-08-03 20:53:05 +0300

    update

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,8 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef binary_tree_t bst_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -52,5 +54,6 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_complete(const binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_search(const bst_t *tree, int value);
 
 #endif
