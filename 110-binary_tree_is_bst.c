da50d379e5db07e1aea0129113b829c1486954c7
thomaskitaba <thomaskitaba@gmail.com>
2023-08-03 16:23:33 +0300

    110 check if the BT is a BST

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,13 +1,14 @@
 # include "binary_trees.h"
 /**
-* binary_tree_is_bst - invert tree to right
+* check_if_bst - invert tree to right
 * @tree: root of the tree
 * Return: 1 if tree is valid BST, 0 otherwise
 */
-int binary_tree_is_bst(const binary_tree_t *tree)
+int check_if_bst(const binary_tree_t *tree)
 {
+	int is_tree;
 
-
+	is_tree = 0;
 	if (tree == NULL)
 	{
 		return (0);
@@ -20,19 +21,39 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 		if (tree->left != NULL)
 		{
 			if (tree->n > tree->left->n)
-				return (1);
-
+			{
+				is_tree++;
+				return (is_tree);
+			}
 			else
-				return (0);
+				return (is_tree);
 		}
-		if (tree->right!= NULL)
+		if (tree->right != NULL)
 		{
 			if (tree->n < tree->right->n)
-				return (1);
+			{
+				is_tree++;
+				return (is_tree);
+			}
 			else
-				return (0);
+				return (is_tree);
 		}
 	}
 
-	return (0);
+	return (is_tree);
+}
+/**
+* binary_tree_is_bst - invert tree to right
+* @tree: root of the tree
+* Return: 1 if tree is valid BST, 0 otherwise
+*/
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (check_if_bst(tree) != 0)
+		return (1);
+	else
+		return (0);
 }
