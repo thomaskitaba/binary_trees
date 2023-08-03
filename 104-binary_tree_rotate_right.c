c387d04d2c2d107759821e89e3640fc8b921fddb
thomaskitaba <thomaskitaba@gmail.com>
2023-08-03 15:12:03 +0300

    added comment to rotate binary tee right

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -7,17 +7,19 @@
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *temp;
-
+    /* check if we have left node*/
 	if (tree == NULL || tree->left == NULL)
 		return (NULL);
-
+    /* set tree->left node as temp */
 	temp = tree->left;
+    /* assign right_grand_child of temp as left child of tree */
 	tree->left = temp->right;
-
+    /* chag*/
 	if (temp->right != NULL)
 	{
 		temp->right->parent = tree;
 	}
+    /* make tree child of  temp*/
 	temp->right = tree;
 	temp->parent = tree->parent;
 	tree->parent = temp;
